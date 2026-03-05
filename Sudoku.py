2b20acd3d08d1aa96d8425197fd481559a20aa2e
Aiman <aim4n.mohd@gmail.com>
2026-03-05 15:11:07 +0530

    Completed

--- Sudoku.py
+++ Sudoku.py
@@ -1,12 +1,3 @@
-board = [[0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0],
-         [0,0,0,0,0,0,0,0,0]]
 
 def cell_check(board, row, col):
     #initialize arrays for keeping track of nums
@@ -34,30 +25,37 @@ def cell_check(board, row, col):
     #check group, starting from top left 
     for i in range(start_row, start_row+3):
         for j in range(start_col, start_col+3):
-            if board[i][j] not in group_arr:
-                group_arr.append(board[i][j])
-            else:
-                return False
+            if board[i][j]!=0:
+                if board[i][j] not in group_arr:
+                    group_arr.append(board[i][j])
+                else:
+                    return False
 
     return True
 
 def check_board(board):
-    #initialize arrays to track
-    col_arr = []
-    row_arr = []
     #2 loops for each col of each row
     for i in range(9):
+        #Initialize arrays for each iteration or row/col
+        col_arr = []
+        row_arr = []
         for j in range(9):
+            #checking for any zeroes
+            if board[i][j] == 0:
+                return False
+            
             #checking rows
             if board[i][j] not in row_arr:
                 row_arr.append(board[i][j])
             else:
                 return False
+            
             #checking cols
             if board[j][i] not in col_arr:
                 col_arr.append(board[j][i])
             else:
                 return False
+            
     #outer loop with step=3 to get the start of each group
     for row in range(0,9,3):
         for col in range(0,9,3):
@@ -70,5 +68,46 @@ def check_board(board):
                         group_arr.append(board[r][c])
                     else:
                         return False
-                    
+
     return True
+
+
+def solve(board, row, col):
+    if row == 9:
+        return True
+    if board[row][col]: #skips the cells with existing/unchangeable values
+        next_row = row + (col//8)
+        next_col = (col+1) % 9
+        return solve(board, next_row, next_col)
+    else:
+        num = 0
+        flag = False
+        while not flag:
+            num+=1
+            if num == 10:
+                board[row][col] = 0
+                break
+            board[row][col] = num
+            if cell_check(board, row, col):
+                flag = solve(board, row+(col//8), (col+1)%9)
+                if flag:
+                    return True
+        return False
+            
+
+
+
+board = []
+print("Enter values row by row: ")
+for i in range(9):
+    row = list(map(int, input().split()))
+    board.append(row)
+
+solved = solve(board, 0, 0)
+
+if solved:
+    print("\n\nSolution:\n")
+    for i in board:
+        print(i)
+else:
+    print("\n\nSOLUTION NOT POSSIBLE!!!")
